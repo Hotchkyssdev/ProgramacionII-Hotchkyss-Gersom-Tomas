53b9dd6f2839d72f58c3058fec65aa03fab84405
Gersom Tomás Hotchkyss <128932163+Hotchkyssdev@users.noreply.github.com>
2026-08-21 09:45:23 -0300

    Clase 1 - Tema 3 - Ej1: primer programa Java

---
+++ Clase1/Tema3_Ej1.java
@@ -0,0 +1,16 @@
+public class Tema3_Ej1 {
+
+/*
+Versiones verificadas desde terminal:
+
+java version "26.0.2" 2026-07-21
+Java(TM) SE Runtime Environment (build 26.0.2+10-55) 
+Java HotSpot(TM) 64-Bit Server VM (build 26.0.2+10-55, mixed mode, sharing)
+
+javac 26.0.2
+*/
+
+    public static void main(String[] args) {
+        System.out.println("Hola, ProgramaciónII!"); 
+    }
+}
